4ce89768d49e364d0ff68cc063356e1bd683517d
Maxime.G <maxime.gilmant@student.junia.com>
2024-01-24 09:50:51 +0100

    correction

--- exo2.cpp
+++ exo2.cpp
@@ -1,35 +1,58 @@
-#include <cstdlib>
 #include <iostream>
+#include <cmath>
+#include <array>
 
-
-bool est_premier(int n){
-    if(n <=1){
-        std::cout << "pas premier" <<std::endl;
+bool est_premier(int n) {
+    if (n <= 1) {
         return false;
     }
-    int i=2;
-    bool premier = true;
 
-    while(premier && i<= n-1){
-        int reste = n%i;
-        if(reste ==0){
-            std::cout << "pas premier" <<std::endl;
-            premier = false;
+    int limite = static_cast<int>(sqrt(n)) + 1;
+    int i = 2;
+
+    while (i < limite) {
+        if (n % i == 0) {
+            return false;
         }
         ++i;
     }
-    if(premier){
-        std::cout << " premier" <<std::endl;
-    }
-    return premier;
+
+    return true;
 }
 
-int main(){
+void premiers_inferieurs(const int limite) {
+    const int taille_tab = 1000;
+    std::array<int, taille_tab> tabPremiers{};
+    int indice = 0;
 
-    const int n = 3;
-    est_premier(n);
+    std::cout << "Liste des nombres premiers inférieurs à " << limite << std::endl;
 
+    for (int nombre = 2; nombre < limite; ++nombre) {
+        if (est_premier(nombre)) {
+            std::cout << nombre << std::endl;
+            tabPremiers[indice++] = nombre;
 
+            if (indice == taille_tab) {
+                std::cout << "Tableau des premiers complets (taille maximale atteinte)." << std::endl;
+                break;
+            }
+        }
+    }
+
+    std::cout << "Tableau des premiers : [";
+    for (int i = 0; i < indice; ++i) {
+        std::cout << tabPremiers[i];
+        if (i < indice - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]" << std::endl;
+}
+
+int main() {
+    const int n = 100;
+    //est_premier(n);
+    premiers_inferieurs(n);
 
     return 0;
 }
